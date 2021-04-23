9c5870eec337bc4f680f2b8d869b26d498e9cc58
Faktor 10 <oreillyross@gmail.com>
2021-04-23 14:10:36 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { Card } from "./components/Card";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Card />
+    </div>
+  );
+}
